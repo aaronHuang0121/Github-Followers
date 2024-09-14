1e0c500bc5adb7698bc7d937de99207f6e6defe2
Aaron Huang <aaron.huang0121@gmail.com>
2024-09-14 14:44:40 +0800

    feat: fetch followers

--- Github Followers/Github Followers/Screens/FollowerListViewController.swift
+++ Github Followers/Github Followers/Screens/FollowerListViewController.swift
@@ -13,8 +13,23 @@ class FollowerListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        configureNavigationBar()
         
+        NetworkManager.shared.getFollowers(username: username, page: 1) { result in
+            switch result {
+            case .success(let result):
+                print(result)
+            case .failure(let error):
+                self.alert(
+                    title: "Bad stuff happened",
+                    message: error.localizedDescription
+                )
+            }
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureNavigationBar()
     }
     
     private func configureNavigationBar() {
