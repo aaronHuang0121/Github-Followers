9678dc6092a557dff29882882031893b1de8ce70
Aaron Huang <aaron.huang0121@gmail.com>
2024-09-14 15:23:01 +0800

    feat: added follower cell

---
+++ Github Followers/Github Followers/Views/Cells/FollowerCell.swift
@@ -0,0 +1,55 @@
+//
+//  FollowerCell.swift
+//  Github Followers
+//
+//  Created by Aaron on 2024/9/14.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseId = "FollowerCell"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        let padding: CGFloat = 8
+        configureImage(padding: padding)
+        configureUsernameLabel(padding: padding)
+    }
+
+    private func configureImage(padding: CGFloat) {
+        addSubview(avatarImageView)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor)
+        ])
+    }
+
+    private func configureUsernameLabel(padding: CGFloat) {
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
