698351263ba91b5a719a6b585e3143ccb1553298
Aaron Huang <aaron.huang0121@gmail.com>
2024-10-01 15:10:44 +0800

    fix: optimized empty state view

--- Github Followers/Github Followers/Views/GFEmptyStateView.swift
+++ Github Followers/Github Followers/Views/GFEmptyStateView.swift
@@ -36,9 +36,12 @@ class GFEmptyStateView: UIView {
         logoImageView.image = UIImage(named: "empty-state")
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         logoImageView.contentMode = .scaleAspectFit
+
+        let labelCenterConstant: CGFloat = UIWindow.current?.screen.bounds.height ?? 0 > 700 ? -200 : -130
+        let logoBottomConstraint: CGFloat = UIWindow.current?.screen.bounds.height ?? 0 > 700 ? 300 : 200
         
         NSLayoutConstraint.activate([
-            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -200),
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: labelCenterConstant),
             messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
             messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
             messageLabel.heightAnchor.constraint(equalToConstant: 200),
@@ -46,7 +49,7 @@ class GFEmptyStateView: UIView {
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1.3),
             logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 240)
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: logoBottomConstraint)
         ])
     }
 }
